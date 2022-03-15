f8ca8a93561e1c2b68390d4953a241e93d86769c
Akbar <shaikakbarbasha38@gmail.com>
2022-03-16 00:34:22 +0530

    search done

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -12,6 +12,10 @@ const Homepage = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const { cartItems } = useSelector((state) => state.cartReducer);
+
+  const [searchkey, setSearchkey] = useState("");
+  const [filtertype, setFiltertype] = useState("");
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -20,7 +24,6 @@ const Homepage = () => {
   }, []);
 
   async function getdata() {
- 
     try {
       setLoading(true);
       const users = await getDocs(collection(fireDB, "products"));
@@ -53,44 +56,66 @@ const Homepage = () => {
   return (
     <Layout loading={loading}>
       <div className="container">
+        <div className="d-flex w-50">
+          <input 
+            type="text"
+            className="form-control m-2"
+            placeholder="search items"
+            value={searchkey}
+            onChange={(e) => setSearchkey(e.target.value)}
+          />
+          <select
+            className="form-control m-2"
+            value={filtertype}
+            onChange={(e) => setFiltertype(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="electronics">Electronics</option>
+            <option value="fashion">Fashion</option>
+            <option value="furniture">Furniture</option>
+          </select>
+        </div>
         <div className="row">
-          {products.map((product) => {
-            return (
-              <div className="col-md-4" key={product.id}>
-                <div className="m-2 p-1 product position-relative">
-                  <div className="product-content">
-                    <p>{product.name}</p>
-                    <div className="text-center">
-                      <img
-                        src={product.imageURL}
-                        alt=""
-                        className="product-img"
-                      />
+          {products
+            .filter((obj) => obj.name.toLowerCase().includes(searchkey))
+            .filter((obj) => obj.category.toLowerCase().includes(filtertype))
+            .map((product) => {
+              return (
+                <div className="col-md-4" key={product.id}>
+                  <div className="m-2 p-1 product position-relative">
+                    <div className="product-content">
+                      <p>{product.name}</p>
+                      <div className="text-center">
+                        <img
+                          src={product.imageURL}
+                          alt=""
+                          className="product-img"
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="product-actions">
-                    <h2>{product.price} RS/-</h2>
-                    <div className="d-flex">
-                      <button
-                        onClick={() => addToCart(product)}
-                        className="mx-2"
-                      >
-                        ADD TO CART
-                      </button>
-                      <button
-                        onClick={() => {
-                          navigate(`/productinfo/${product.id}`);
-                        }}
-                      >
-                        VIEW
-                      </button>
+                    <div className="product-actions">
+                      <h2>{product.price} RS/-</h2>
+                      <div className="d-flex">
+                        <button
+                          onClick={() => addToCart(product)}
+                          className="mx-2"
+                        >
+                          ADD TO CART
+                        </button>
+                        <button
+                          onClick={() => {
+                            navigate(`/productinfo/${product.id}`);
+                          }}
+                        >
+                          VIEW
+                        </button>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </div>
       </div>
     </Layout>
