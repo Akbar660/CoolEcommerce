6f7da1162674eb15447f62aa9ff728c0190c2b17
Akbar <shaikakbarbasha38@gmail.com>
2022-03-15 17:43:32 +0530

    cartpage done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {FaBars} from "react-icons/fa"
+import {FaBars,FaCartPlus} from "react-icons/fa"
 import { useSelector } from 'react-redux'
 
 const Header = () => {
@@ -27,7 +27,7 @@ const {cartItems}=useSelector(state=>state.cartReducer)
           <Link className="nav-link" to="/">logout</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/">cart {cartItems.length}</Link>
+          <Link className="nav-link" to="/cart"><FaCartPlus /> {cartItems.length}</Link>
         </li>
       </ul>
     </div>
--- src/pages/Cartpage.js
+++ src/pages/Cartpage.js
@@ -1,11 +1,76 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Layout from "../components/Layout";
+import { useSelector, useDispatch } from "react-redux";
+
+import { FaTrash } from "react-icons/fa";
+
 const Cartpage = () => {
+  const [total,setTotal]=useState(0)
+console.log(total)
+  const { cartItems } = useSelector((state) => state.cartReducer);
+  // console.log(cartItems)
+
+  const dispatch = useDispatch();
+
+useEffect(()=>{
+  let sum=0;
+  cartItems.forEach(cartItem=>{
+   sum=sum+(+cartItem.price)
+  })
+  setTotal(sum)
+
+},[cartItems])
+
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const deleteFromCart = (product) => {
+    dispatch({ type: "DELETE_FROM_CART", payload: product });
+  };
+
   return (
     <Layout>
-      <h1>cart</h1>
-      </Layout>
-  )
-}
+      <table className="table mt-3">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cartItems.map((item) => {
+            return (
+              <tr>
+                <td>
+                  <img src={item.imageURL} height="80" width="80" />
+                </td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>
+                  <FaTrash
+                    cursor="pointer"
+                    onClick={() => deleteFromCart(item)}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
-export default Cartpage
+      <div className="d-flex justify-content-end">
+            <h1 className="total-amount">Totalamount={total} RS/-</h1>
+      </div>
+      <div className="d-flex justify-content-end mt-3">
+           <button>PLACE ORDER</button>
+      </div>
+    </Layout>
+  );
+};
+
+export default Cartpage;
